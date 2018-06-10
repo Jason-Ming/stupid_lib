eb0890b46051e29d3f653b8273b3f5b543f48801
Jason <mingliang37@163.com>
2018-06-10 21:57:24 +0800

    update

--- include/s_defines.h
+++ include/s_defines.h
@@ -20,18 +20,13 @@ typedef enum TAG_ENUM_BOOLEAN
 #define FREE(p) do{ if(p != NULL){free(p);}}while(0);
 #define MIN(x, y) ((x) < (y)? (x):(y))
 #define SIZE_OF_ARRAY(a) (sizeof(a)/sizeof(a[0]))
+#define PRIVATE static
 
 /* GET_BITS: get n bits from position p */ 
 #define GET_BITS(x, p, n) (x & ((~(~0 << n)) << (p+1-n)))
-//1000 1011 p = 2 n = 2
-//      **
-//0101 1110
-//1000 1101
 #define SET_BITS(x, p, n, y) ((x & (~((~(~0 << n)) << (p+1-n)))) | ( (y & ~(~0 << n)) << (p+1-n)))
-
 #define INVERT_BITS(x, p, n) ((x & (~((~(~0 << n)) << (p+1-n)))) | ((~x) & ((~(~0 << n)) << (p+1-n))))
-
-#define PRIVATE static
+#define RIGHT_ROT(x, n) ((sizeof(x)*8 == n)?(x):(((x >> n) & (~(~0 << (sizeof(x)*8 - n)))) | (x << (sizeof(x)*8 - n))))
 
 #endif
 
